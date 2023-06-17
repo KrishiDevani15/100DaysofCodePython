cefd34d905910b4ea924c597f44e57c606cf06c5
Krishi.Devani <92908325+KrishiDevani15@users.noreply.github.com>
2023-06-17 11:53:43 +0530

    Add files via upload

---
+++ 100DaysOfCodePython/Day-1/Band_Name_Generator.py
@@ -0,0 +1,5 @@
+#Go to: https://replit.com/@appbrewery/band-name-generator-start?v=1
+print("Welcome to Band name Generator")
+city = input("Enter your city name: ")
+pet = input("Enter your pet name: ")
+print("Your band name is: " + city + " " + pet)
---
+++ 100DaysOfCodePython/Day-1/Exercise1.py
@@ -0,0 +1,18 @@
+print("Hello World!")
+print(6)
+print(5.6)
+print(True)
+print("India","Pakistan","Nepal","Srilanka")
+print("India",5,False)
+
+# Using Sep
+print("India","Pakistan","Nepal","SriLanka",sep='/')
+print("India","Pakistan","Nepal","SriLanka",sep='-')
+print("India","Pakistan","Nepal","SriLanka",sep='')
+
+print("Hello")
+print("World")
+
+#end function for new lines
+print("Hello",end=' ')
+print("World")
